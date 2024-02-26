9a50690cc15a904a1a50b4981611c27cf5c0b7de
OXYGEN XD <129831825+PRADHAN474@users.noreply.github.com>
2024-02-26 14:50:50 +0530

    Update userbot.py

--- ANNIEMUSIC/core/userbot.py
+++ ANNIEMUSIC/core/userbot.py
@@ -11,35 +11,35 @@ assistantids = []
 class Userbot(Client):
     def __init__(self):
         self.one = Client(
-            name="Annie1",
+            name="oxygen1",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING1),
             no_updates=True,
         )
         self.two = Client(
-            name="Annie2",
+            name="oxygen2",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING2),
             no_updates=True,
         )
         self.three = Client(
-            name="Annie3",
+            name="oxygen3",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING3),
             no_updates=True,
         )
         self.four = Client(
-            name="Annie4",
+            name="oxygen4",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING4),
             no_updates=True,
         )
         self.five = Client(
-            name="Annie5",
+            name="oxygen5",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING5),
@@ -47,18 +47,18 @@ class Userbot(Client):
         )
 
     async def start(self):
-        LOGGER(__name__).info(f"Annie's Assistant starting...")
+        LOGGER(__name__).info(f"oxygen's Assistant starting...")
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("doraworld5")
+                await self.one.join_chat("CHATTING_2024")
               
-                await self.one.join_chat("JARVIS_HUB")
+                await self.one.join_chat("+H5IrqVsgKYU3ODI1")
             except:
                 pass
             assistants.append(1)
             try:
-                await self.one.send_message(config.LOGGER_ID, "Annie's Assistant Start.........")
+                await self.one.send_message(config.LOGGER_ID, "oxygen's Assistant Start.........")
                                          
             except:
                 LOGGER(__name__).error(
@@ -74,14 +74,14 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("doraworld5")
+                await self.two.join_chat("+H5IrqVsgKYU3ODI1")
                 
-                await self.one.join_chat("JARVIS_HUB")
+                await self.one.join_chat("CHATTING_2024")
             except:
                 pass
             assistants.append(2)
             try:
-                await self.two.send_message(config.LOGGER_ID, "Annie's Assistant Started")
+                await self.two.send_message(config.LOGGER_ID, "oxygen Assistant Started")
             except:
                 LOGGER(__name__).error(
                     "Assistant Account 2 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin!"
@@ -96,14 +96,14 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("doraworld5")
+                await self.three.join_chat("+H5IrqVsgKYU3ODI1")
                 
-                await self.one.join_chat("JARVIS_HUB")
+                await self.one.join_chat("CHATTING_2024")
             except:
                 pass
             assistants.append(3)
             try:
-                await self.three.send_message(config.LOGGER_ID, "Annie's Assistant Started")
+                await self.three.send_message(config.LOGGER_ID, "oxygen Assistant Started")
             except:
                 LOGGER(__name__).error(
                     "Assistant Account 3 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin! "
@@ -118,14 +118,14 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("doraworld5")
+                await self.four.join_chat("+H5IrqVsgKYU3ODI1")
                 
-                await self.one.join_chat("JARVIS_HUB")
+                await self.one.join_chat("CHATTING_2024")
             except:
                 pass
             assistants.append(4)
             try:
-                await self.four.send_message(config.LOGGER_ID, "Annie's Assistant Started")
+                await self.four.send_message(config.LOGGER_ID, "oxygen Assistant Started")
             except:
                 LOGGER(__name__).error(
                     "Assistant Account 4 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin! "
@@ -140,14 +140,14 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("doraworld5")
+                await self.five.join_chat("+H5IrqVsgKYU3ODI1")
                 
-                await self.one.join_chat("JARVIS_HUB")
+                await self.one.join_chat("CHATTING_2024")
             except:
                 pass
             assistants.append(5)
             try:
-                await self.five.send_message(config.LOGGER_ID, "Annie's Assistant Started")
+                await self.five.send_message(config.LOGGER_ID, "oxygen Assistant Started")
             except:
                 LOGGER(__name__).error(
                     "Assistant Account 5 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin! "
